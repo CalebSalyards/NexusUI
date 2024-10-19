794632b2fd697eaed8241408c0065fa9454c40ad
Gerrit Thompson <gethompson595@gmail.com>
2024-10-18 19:54:30 -0600

    added a generateCourses function

--- sign-in/scripts/script.js
+++ sign-in/scripts/script.js
@@ -1,5 +1,8 @@
 const signedIn = false;
 const signInButton = document.getElementById('sign-in');
+const url = 'Something should be here maybe';
+const courses = 'find out what the courses page is';
+
 
 async function signInCheck(signedIn) {
     if(signedIn){
@@ -7,6 +10,18 @@ async function signInCheck(signedIn) {
     } else{
 
     }
-}
+};
 
 signInCheck(signedIn)
+
+async function generateCourses(){
+    const main = document.getElementById('mainPage');
+    const coursesList = document.createElement('ul');
+    main.appendChild(coursesList)
+    forEach((url.courses), () => {
+        if(url.courses.role == 'teacher' || url.courses.role == 'TA'){
+            const courseSection = document.createElement('section');
+            coursesList.appendChild(courseSection);
+        }
+    })
+}
